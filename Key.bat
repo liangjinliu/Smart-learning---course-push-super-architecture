42e90d9bb73d4a060a6d65c1ade1917aff0cff40
liangjinliu <wjllnh@163.com>
2023-05-18 09:07:08 +0800

    add Smart-learning---course-push-super-architecture

---
+++ Key.bat
@@ -0,0 +1,6 @@
+@echo CC-port account name and password
+
+cmdkey /generic:ZX_ICS /user:RPA00002 /pass:ctfics2022
+
+
+cmdkey /generic:ZX_Web /user:core /pass:123456
---
+++ Smart learning.bat
@@ -0,0 +1,2 @@
+cd "C:\Users\HRIS-RPA\AppData\Local\UiPath\app-21.4.4\"
+UiRobot execute --file "D:\CN-VA Project\Robot instruction path\10072292\Smart learning - course push super architecture\Main.xaml"
